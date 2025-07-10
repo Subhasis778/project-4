93317e7edd3fb6747f04832330779d8764d88e77
Subhasis778 <sroutray890@gmail.com>
2025-07-10 22:51:23 +0530

    Update skill.jsx

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -11,22 +11,27 @@ const skills = () => {
   ];
 
   return (
-    <div className="skills-container">
-      <h2>Skills</h2>
-      <div className="skills-list">
-        {skills.map((skill, index) => (
-          <div key={index} className="skill-item">
-            <div className="skill-header">
-              <span>{skill.name}</span>
-              <span>{skill.level}%</span>
+    <section id="skill">
+      <div className="skills-container">
+        <h2>Skills</h2>
+        <div className="skills-list">
+          {skills.map((skill, index) => (
+            <div key={index} className="skill-item">
+              <div className="skill-header">
+                <span>{skill.name}</span>
+                <span>{skill.level}%</span>
+              </div>
+              <div className="progress-bar">
+                <div
+                  className="progress-fill"
+                  style={{ width: `${skill.level}% ` }}
+                ></div>
+              </div>
             </div>
-            <div className="progress-bar">
-              <div className="progress-fill" style={{ width: `45% ` }}></div>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
