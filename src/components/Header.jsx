2acf39d12191143e7bb0f7f5254fc4b8930652e0
Subhasis778 <sroutray890@gmail.com>
2025-07-10 22:36:45 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,13 @@ const Header = () => {
   return (
     <>
       <div className="navbar">
-        <nav class="navbar navbar-expand-lg navbar-light  bg-light">
-          <div
-            className="container-fluid port position-fixed
-         "
-          >
-            <a class="navbar-brand text-white ">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid port">
+            <a className="navbar-brand text-white">
               <b>Subhasis Routray</b>
             </a>
-            <pre> </pre>
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarNavAltMarkup"
@@ -25,27 +21,27 @@ const Header = () => {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navbar-nav ms-auto">
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav ms-auto">
                 <a
-                  class="nav-link active text-white"
+                  className="nav-link active text-white"
                   aria-current="page"
                   href="#"
                 >
                   Home
                 </a>
-                <Link to="#about" class="nav-link text-white">
+                <Link to="#about" className="nav-link text-white">
                   About
                 </Link>
-                <Link to="#skill" class="nav-link  text-white">
+                <Link to="#skill" className="nav-link text-white">
                   Skills
                 </Link>
-                <Link id="#project" class="nav-link  text-white">
+                <Link to="#project" className="nav-link text-white">
                   Projects
                 </Link>
-                <Link id="#contact" class="nav-link text-white">
+                <Link to="#contact" className="nav-link text-white">
                   Contact
                 </Link>
               </div>
